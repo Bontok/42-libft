4f655ea5cabdafef3222dc7608ff1a23aa5eb5ee
Bontok <email@example.com>
2022-10-15 06:07:30 +0200

    ft digit2

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: FedericoOrlando <FedericoOrlando@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 05:58:34 by FedericoOrl       #+#    #+#             */
-/*   Updated: 2022/10/15 05:59:02 by FedericoOrl      ###   ########.fr       */
+/*   Updated: 2022/10/15 06:07:08 by FedericoOrl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,3 @@ int ft_isdigit(int c)
     return (1);
     return (0);
 }
-#include <stdio.h>
-#include <ctype.h>
-
-int main()
-{
